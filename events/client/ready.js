b2932eaccf5cdf6b55b6970bf4324859b6be1657
Colderry <72723052+Colderry@users.noreply.github.com>
2021-03-04 19:54:09 +0600

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ const chalk = require("chalk")
 
 module.exports = async (client) => {
  
- console.log(`━━━━━━━━━━━━━━━━━━━━━━━`);
+ console.log(`━━━━━━━━━━━━━━━━━━━━━━━`)
  console.log(`┏━━━━━━━━━━━━━━━━━━━━━┓`)
  console.log(`┃ AiChan is now Online┃`)
  console.log(`┗━━━━━━━━━━━━━━━━━━━━━┛`)
