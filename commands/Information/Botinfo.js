969d390863070c53febb01ddffe6a130b498503b
Colderry <72723052+Colderry@users.noreply.github.com>
2021-03-05 12:30:07 +0600

    Update Botinfo.js

--- commands/Information/Botinfo.js
+++ commands/Information/Botinfo.js
@@ -1,9 +1,9 @@
-const { MessageEmbed, version: djsversion } = require("discord.js")
+const { MessageEmbed, version: djsversion } = require("discord.js");
 
-const { name, version } = require("../../package.json")
+const { version } = require("../../package.json");
 
-const moment = require("moment")
-require('moment-duration-format')
+const moment = require("moment");
+require('moment-duration-format');
 
 module.exports = {
 
@@ -19,19 +19,19 @@ module.exports = {
 
   run: async (client, message, args) => {
 
-    const servers = client.guilds.cache.size.toLocaleString()
+    const servers = client.guilds.cache.size.toLocaleString(); // Total servers the AiChan in,
 
-    const users = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()
+    const users = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString(); // Total users of every server that the AiChan in,
 
-    const channels = client.channels.cache.size.toLocaleString()
+    const channels = client.channels.cache.size.toLocaleString(); // Total channels of every server that the bot in
 
-    const commands = client.commands.size.toLocaleString();
+    const commands = client.commands.size.toLocaleString(); // Total number of commands that AiChan have.
 
-    const createdAt = moment(client.user.createdTimestamp).format('ddd, MMM Do YYYY')
+    const createdAt = moment(client.user.createdTimestamp).format('ddd, MMM Do YYYY'); // The date when AiChan has been created. Used moment package for better format
 
-    const uptime = moment.duration(client.uptime).format("D [ days] h[ hours] m[ minutes] s[ seconds]")
+    const uptime = moment.duration(client.uptime).format("D [ days] h[ hours] m[ minutes] s[ seconds]"); // Uptime of AiChan
 
-    
+
     const AiChan = new MessageEmbed()
 
       .setAuthor(`AiChan`, client.user.displayAvatarURL())
@@ -58,14 +58,19 @@ module.exports = {
         {
           name: "Developer",
 
-          value: `[<@!538627730921619486>]`,
+          value: `<@!538627730921619486>`, // You can add your id too :)
 
           inline: false
+        },
+        {
+          name: "** **", // We used ** ** So it won't display it.
+
+          value: "[Invite](https://discord.com/oauth2/authorize?client_id=798290520794857482&scope=bot&permissions=8) | [Support Server](https://discord.gg/NTvrRd72WM) | [Source](https://github.com/Colderry/AiChan)" // You can replace or add your own links. Remember to make like [Your Text](Your Link)
         }
       )
       .setTimestamp()
 
-    message.channel.send(AiChan)
+    message.channel.send(AiChan) // Sends the message embed
 
   }
 }
